836674a3b268a11e1e9cea92c3ed8fda73a5e40a
SUJITHGanesh <144425530+SUJITHGanesh@users.noreply.github.com>
2024-10-08 17:40:29 +0530

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Management System</title>
+    <link rel="stylesheet" href="assets/styles.css">
+</head>
+<body>
+
+<h1>Student Management System</h1>
+<a href="students/create.php">Add New Student</a> | 
+<a href="students/read.php">View All Students</a>
+
+</body>
+</html>
